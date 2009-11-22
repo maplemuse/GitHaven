7390fbf9f44a839f2485525e7be9368947d5489a
Benjamin C Meyer <ben@meyerhome.net>
2009-11-21 21:38:29 -0500

    redirect to login after signin

--- web/app/controllers/users_controller.rb
+++ web/app/controllers/users_controller.rb
@@ -67,7 +67,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         flash[:notice] = "User #{@user.username} was successfully created."
-        format.html { redirect_to(@user) }
+        format.html { redirect_to(:action => 'login') }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
