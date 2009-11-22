30933c30d680e8f5202443a1d6072fb306b7649f
Benjamin C Meyer <ben@meyerhome.net>
2009-11-21 21:20:48 -0500

    Add the repo migration

---
+++ web/db/migrate/20091122004036_create_repositories.rb
@@ -0,0 +1,16 @@
+class CreateRepositories < ActiveRecord::Migration
+  def self.up
+    create_table :repositories do |t|
+      t.integer :owner_id, :null => false, :options => "CONSTRAINT fk_line_item_users REFERENCES users(id)"
+      t.string :name
+      t.string :description
+      t.string :defaultbranch
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :repositories
+  end
+end
