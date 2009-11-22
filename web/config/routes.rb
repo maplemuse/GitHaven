d3e6dda81afae62008de2bf6d89ad8fbc5c8db4b
Benjamin C Meyer <ben@meyerhome.net>
2009-11-21 19:09:56 -0500

    Add basic user authentication - login/logout/signup

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -40,6 +40,10 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
+  map.connect 'login', :controller => 'users', :action => 'login'
+  map.connect 'logout', :controller => 'users', :action => 'logout'
+  map.connect 'signup', :controller => 'users', :action => 'new'
+ 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
