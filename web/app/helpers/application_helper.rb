4ec3ec69b4a8b19511aef278288c318192b1ee97
Benjamin C Meyer <ben@meyerhome.net>
2009-11-26 01:57:42 -0500

    Create a way to set the page title's

--- web/app/helpers/application_helper.rb
+++ web/app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+def page_title
+  (@content_for_title + " &mdash; " if @content_for_title).to_s + 'GitForest'
+end
+
+def page_heading(text)
+  content_tag(:h1, content_for(:title){ text })
+end
+
 end
