2f1a77a069902cc28cb79b6fd0641499a2a0b359
Benjamin C Meyer <ben@meyerhome.net>
2009-11-26 03:21:58 -0500

    Add authorization to the permissions controller and translate the contoller strings

--- web/app/controllers/permissions_controller.rb
+++ web/app/controllers/permissions_controller.rb
@@ -1,10 +1,15 @@
 class PermissionsController < ApplicationController
   before_filter :require_login
+  before_filter :require_authorization, :except => [:new, :create]
 
   # GET /permissions/new
-  # GET /permissions/new.xml
   def new
     @repository = Repository.find(params[:repo])
+    if @repository.user != @user
+        redirect_to(edit_repository_url(@repository))
+        return
+    end
+
     @permission = Permission.new
     @permission.repository = @repository
 
@@ -14,58 +19,71 @@ class PermissionsController < ApplicationController
     end
   end
 
-  # GET /permissions/1/edit
-  def edit
-    @permission = Permission.find(params[:id])
-  end
-
   # POST /permissions
-  # POST /permissions.xml
   def create
     @permission = Permission.new(params[:permission])
+    return if !require_authorization
+
     @repository = @permission.repository
     @permission.user_id = User.find_by_username(params[:user]).id
     @repository.permissions << @permission
     respond_to do |format|
       if @repository.save && @permission.save
-        flash[:notice] = 'Permission was successfully created.'
+        flash[:notice] = t('permissions.created',
+                            :username => h(@permission.user.username),
+                            :repositoryname => h(@permission.repository.name))
         format.html { redirect_to(edit_repository_url(@repository)) }
         format.xml  { render :xml => @permission, :status => :created, :location => @permission }
       else
-        format.html { render :action => "new" }
+        format.html { render :action => 'new' }
         format.xml  { render :xml => @permission.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PUT /permissions/1
-  # PUT /permissions/1.xml
-  def update
-    @permission = Permission.find(params[:id])
+  # GET /permissions/1/edit
+  def edit
+  end
 
+  # PUT /permissions/1
+  def update
     respond_to do |format|
       if @permission.update_attributes(params[:permission])
-        flash[:notice] = 'Permission was successfully updated.'
+        flash[:notice] = t('permissions.updated',
+                            :username => h(@permission.user.username),
+                            :repositoryname => h(@permission.repository.name))
         format.html { redirect_to(edit_repository_url(@permission.repository)) }
         format.xml  { head :ok }
       else
-        format.html { render :action => "edit" }
+        format.html { render :action => 'edit' }
         format.xml  { render :xml => @permission.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /permissions/1
-  # DELETE /permissions/1.xml
   def destroy
-    @permission = Permission.find(params[:id])
+    username = @permission.user.username
     @repository = @permission.repository
     @permission.destroy
-
+    flash[:notice] = t('permissions.deleted',
+                        :username => h(username),
+                        :repositoryname => h(@repository.name))
     respond_to do |format|
       format.html { redirect_to(edit_repository_url(@repository)) }
       format.html { redirect_to(permissions_url) }
       format.xml  { head :ok }
     end
   end
+
+private
+  def require_authorization
+    @permission = Permission.find(params[:id]) if !@permission
+    if @permission.repository.user != @user
+        redirect_to(edit_repository_url(@permission.repository))
+        return false
+    end
+    return true
+  end
+
 end
