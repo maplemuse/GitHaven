c699ead89ece9bcda9b3d2fcb6442a7c91ba59b7
Benjamin C Meyer <ben@meyerhome.net>
2009-11-26 04:02:07 -0500

    Escape the title string

--- web/app/helpers/application_helper.rb
+++ web/app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
 
 def page_title
-  (@content_for_title + " &mdash; " if @content_for_title).to_s + 'GitForest'
+  (h(@content_for_title) + " &mdash; " if @content_for_title).to_s + 'GitForest'
 end
 
 def page_heading(text)
