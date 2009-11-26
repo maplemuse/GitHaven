d5911e3fd13d4cabb961236489c283e523a29940
Benjamin C Meyer <ben@meyerhome.net>
2009-11-26 02:42:30 -0500

    Cleanup authorization

--- web/app/controllers/repositories_controller.rb
+++ web/app/controllers/repositories_controller.rb
@@ -1,13 +1,8 @@
 require 'grit'
 
 class RepositoriesController < ApplicationController
-  before_filter :require_login, :except => [:index, :show, :commits, :commit]
-
-  def location
-    config = Rails::Configuration.new
-    location = config.root_path + '/../repos/' + @repository.user.username + '/' + @repository.name + '.git'
-    return location
-  end
+  before_filter :require_login, :only => [:new, :create, :edit, :update, :destroy]
+  before_filter :requires_authorization, :only => [:edit, :update, :destroy]
 
   # GET /repositories
   # GET /repositories.xml
@@ -20,25 +15,6 @@ class RepositoriesController < ApplicationController
       format.html # index.html.erb
       format.xml  { render :xml => @repositories }
     end
-  end
-
-  def find_repository
-    if params[:repo]
-      @repository = Repository.find_by_name(params[:repo])
-    else
-      @repository = Repository.find(params[:id])
-    end
-    find_owner
-    if !@owner
-      flash[:notice] = "Sorry, but the repository does not exist."
-      redirect_to :action => 'index'
-      return false;
-    end
-    @host = request.host
-    @repo = Grit::Repo.new(location())
-    return true
-    rescue
-    return true
   end
 
   # GET /repositories/1
@@ -74,9 +50,7 @@ class RepositoriesController < ApplicationController
   end
 
   def commit
-    if !find_repository
-      return
-    end
+    return if !find_repository
     if !params[:sha1]
       redirect_to :action => 'index'
     end
@@ -99,15 +73,6 @@ class RepositoriesController < ApplicationController
     end
   end
 
-  # GET /repositories/1/edit
-  def edit
-    @repository = Repository.find(params[:id])
-    find_owner
-    if !check_authorization
-      return
-    end
-  end
-
   # POST /repositories
   # POST /repositories.xml
   def create
@@ -125,15 +90,13 @@ class RepositoriesController < ApplicationController
     end
   end
 
+  # GET /repositories/1/edit
+  def edit
+  end
+
   # PUT /repositories/1
   # PUT /repositories/1.xml
   def update
-    @repository = Repository.find(params[:id])
-    find_owner
-    if !check_authorization
-      return
-    end
-
     respond_to do |format|
       if @repository.update_attributes(params[:repository])
         flash[:notice] = t('repository.updated', :name => h(@repository.name))
@@ -149,11 +112,6 @@ class RepositoriesController < ApplicationController
   # DELETE /repositories/1
   # DELETE /repositories/1.xml
   def destroy
-    @repository = Repository.find(params[:id])
-    find_owner
-    if check_authorization == false
-      return
-    end
     name = @repository.name
     @repository.destroy
     flash[:notice] = t('repository.deleted', :name => h(name))
@@ -165,18 +123,36 @@ class RepositoriesController < ApplicationController
   end
 
 private
-  def check_authorization
-    if @owner != @user
-        flash[:notice] = "You do not have permissions to modify this repository."
-        redirect_to :action => 'index'
-        return false
+  def location
+    config = Rails::Configuration.new
+    location = config.root_path + '/../repos/' + @repository.user.username + '/' + @repository.name + '.git'
+    return location
+  end
+
+  def find_repository
+    if params[:repo]
+      @repository = Repository.find_by_name(params[:repo])
+    else
+      @repository = Repository.find(params[:id])
     end
+    @owner = @repository.user
+    if !@owner
+      flash[:notice] = "Sorry, but the repository does not exist."
+      redirect_to root_url
+      return false;
+    end
+    @host = request.host
+    @repo = Grit::Repo.new(location())
+    return true
+    rescue
     return true
   end
 
-  def find_owner
-    if @repository
-      @owner = @repository.user
+  def requires_authorization
+    return if !find_repository
+    if @owner != @user
+        flash[:notice] = "You do not have permissions to modify this repository."
+        redirect_to root_url
     end
   end
 
