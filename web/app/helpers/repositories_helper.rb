96803eb1a16cd65980cb7999995b66c3c7a7dfe2
Benjamin C Meyer <ben@meyerhome.net>
2009-11-29 19:26:40 -0500

    Put the repo box at the top of the repo pages clean up the box

--- web/app/helpers/repositories_helper.rb
+++ web/app/helpers/repositories_helper.rb
@@ -8,4 +8,13 @@ module RepositoriesHelper
     link += "\">" + h(name) + "</a>"
     link
   end
+
+  def link_to_user(user)
+    link_to h(user.username), :controller => 'users', :action => 'show', :user => user.username
+  end
+
+  def link_to_repository(repository)
+    link_to h(repository.name), :controller => 'repositories', :action => 'show', :repo => repository.name
+  end
+
 end
