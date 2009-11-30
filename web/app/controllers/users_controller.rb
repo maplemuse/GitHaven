8bb14184ef2114fb9c669ecdda0a995b79908e1f
Benjamin C Meyer <ben@meyerhome.net>
2009-11-30 00:22:03 -0500

    Fix due to @user -> @loggedinuser change

--- web/app/controllers/users_controller.rb
+++ web/app/controllers/users_controller.rb
@@ -88,12 +88,12 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @loggedinuser.update_attributes(params[:user])
-        flash[:notice] = t('user.updated', :username => h(@user.username))
-        format.html { redirect_to(@user) }
+        flash[:notice] = t('user.updated', :username => h(@loggedinuser.username))
+        format.html { redirect_to(@loggedinuser) }
         format.xml  { head :ok }
       else
         format.html { render :action => 'edit' }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @loggedinuser.errors, :status => :unprocessable_entity }
       end
     end
   end
