044626a4956f1bcbde0d1ea07f1e293c8a095eb4
Benjamin C Meyer <ben@meyerhome.net>
2009-12-02 02:34:54 -0500

    Add a debian directory and script to generate a release

---
+++ debian/githaven.default.ex
@@ -0,0 +1,10 @@
+# Defaults for githaven initscript
+# sourced by /etc/init.d/githaven
+# installed at /etc/default/githaven by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
