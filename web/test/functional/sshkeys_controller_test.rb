d9ca05fc7cb7c22551d2b6070403ed38a50f3ad4
Benjamin C Meyer <ben@meyerhome.net>
2009-12-05 17:06:43 -0500

    Implement a few basic functional sshkey tests

--- web/test/functional/sshkeys_controller_test.rb
+++ web/test/functional/sshkeys_controller_test.rb
@@ -1,45 +1,74 @@
 require 'test_helper'
 
 class SshkeysControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:sshkeys)
-  end
+  fixtures :users
+  fixtures :sshkeys
 
-  test "should get new" do
+  test 'new' do
     get :new
+    assert_not_nil flash[:notice]
+    assert_redirected_to 'login'
+  end
+  test 'new_w_login' do
+    get :new, {}, {:user_id => users(:one).id}
+    assert_nil flash[:notice]
+    assert_not_nil assigns(:sshkey)
     assert_response :success
+    assert_template 'new'
   end
 
   test "should create sshkey" do
-    assert_difference('Sshkey.count') do
+    assert_difference('Sshkey.count', 0) do
       post :create, :sshkey => { }
     end
-
-    assert_redirected_to sshkey_path(assigns(:sshkey))
+    assert_not_nil flash[:notice]
+    assert_redirected_to 'login'
   end
-
-  test "should show sshkey" do
-    get :show, :id => sshkeys(:one).id
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => sshkeys(:one).id
-    assert_response :success
-  end
-
-  test "should update sshkey" do
-    put :update, :id => sshkeys(:one).id, :sshkey => { }
-    assert_redirected_to sshkey_path(assigns(:sshkey))
-  end
-
-  test "should destroy sshkey" do
-    assert_difference('Sshkey.count', -1) do
-      delete :destroy, :id => sshkeys(:one).id
+  test "create_w_login" do
+    assert_difference('Sshkey.count') do
+      post :create, {:sshkey => {:name => 'Default', :key => 'x' }}, {:user_id => users(:one).id}
     end
 
-    assert_redirected_to sshkeys_path
+    assert_not_nil flash[:notice]
+    assert_redirected_to :controller => 'users', :user => users(:one).username
+  end
+
+  test "edit" do
+    get :edit, :id => sshkeys(:one).id
+    assert_not_nil flash[:notice]
+    assert_redirected_to 'login'
+  end
+  test "edit_w_login" do
+    get :edit, {:id => sshkeys(:one).id}, {:user_id => users(:one).id}
+    assert_response :success
+    assert_nil flash[:notice]
+    assert_not_nil assigns(:sshkey)
+  end
+
+  test "update" do
+    get :update, :id => sshkeys(:one).id
+    assert_not_nil flash[:notice]
+    assert_redirected_to 'login'
+  end
+  test "update_w_login" do
+    get :update, {:id => sshkeys(:one).id, :sshkey => {}}, {:user_id => users(:one).id}
+    assert_not_nil flash[:notice]
+    assert_redirected_to :controller => 'users', :user => users(:one).username
+  end
+
+  test "destroy" do
+    assert_difference('Sshkey.count', 0) do
+      delete :destroy, :id => sshkeys(:one).id
+    end
+    assert_not_nil flash[:notice]
+    assert_redirected_to 'login'
+  end
+  test "destroy_w_login " do
+    assert_difference('Sshkey.count', -1) do
+      delete :destroy, {:id => sshkeys(:one).id}, {:user_id => users(:one).id}
+    end
+
+    assert_not_nil flash[:notice]
+    assert_redirected_to :controller => 'users', :user => users(:one).username
   end
 end
